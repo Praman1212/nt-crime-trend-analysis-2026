c814e3d9655fd292fb2947418cb5af2d85f3e74a
Praman1212 <Pramanghimire25@gmail.com>
2026-05-08 12:29:33 +0930

    nt crime analysis project setup

---
+++ nt_crime_analysis.py
@@ -0,0 +1,11 @@
+# pandas is a library that lets you work with tables of data
+# Just like Excel — rows, columns, filters, sorting
+# Without pandas, you cannot read a CSV file in Pythonimport pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import warnings
+
+warnings.filterwarnings("ignore")
+print("All libraries imported successfully")
